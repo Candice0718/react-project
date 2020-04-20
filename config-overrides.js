e67174ae53de035d321ed2cc783e2570792dbe42
Candice <wuqiong910718@163.com>
2020-04-20 16:14:54 +0800

    react项目初始化

---
+++ config-overrides.js
@@ -0,0 +1,16 @@
+const { override, fixBabelImports, addLessLoader } = require('customize-cra');
+
+module.exports = override(
+  fixBabelImports("import", { // antd按需加载
+    libraryName: "antd",
+    libraryDirectory: "es",
+    style: true  // css只加载.css文件，true加载 .css、.less、.sass文件
+  }),
+  addLessLoader({
+    javascriptEnabled: true,
+    modifyVars: { 
+      "@primary-color": "red",
+      "@link-color": "orange"
+    }
+  })
+);
